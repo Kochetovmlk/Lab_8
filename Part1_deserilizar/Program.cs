af62ba0fd85851ca977b82f006516e01fccb7c42
Kochetovmlk <mikhail.kocetov.04@list.ru>
2024-01-08 19:28:34 +0300

    first commit

---
+++ Part1_deserilizar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Xml.Serialization;
+using Animal;
+
+
+namespace Part1_deserilizar
+{
+    class Programm
+    {
+        static void Main()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Pig));
+
+            using (FileStream fs = new FileStream("C:\\Users\\mikha\\OneDrive\\Рабочий стол\\Лабораторные работы по АЯ\\Lab_08\\Part1_deserilizar\\test.xml", FileMode.Open))
+            {
+                Pig pig = xmlSerializer.Deserialize(fs) as Pig;
+                Console.WriteLine($"Type: {pig?.WhatAnimal}, Name: {pig?.Name}, Country: {pig?.Country}, Hide from: {pig.HideFromOtherAnimal}, Favorite food: {pig.FavoriteFood}");
+            }
+        }
+    }
+}
